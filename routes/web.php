d50861509cd3822e1c53b5afebb2afe51ea70bf1
Nectolink <nectolink@gmail.com>
2019-04-26 19:04:46 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('getLeave','Controller@getLeave');
 
 //All post APIs.
 Route::match(['get', 'post'],'postLeave','Controller@postLeave');
+Route::match(['get', 'post'],'sendMessage','Controller@sendMessage');
 Route::match(['get', 'post'],'employeeLeaveStatus/{id}','Controller@employeeLeaveStatus');
 Route::match(['get', 'post'],'LeaveStatusChange/{id}/{status}','Controller@LeaveStatusChange');
 
